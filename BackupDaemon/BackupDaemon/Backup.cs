cf3655d5910738e2be4c9e0d0db98a59c72feb49
novakpatrik <novakpatrik@L06PC15>
2017-04-24 13:27:54 +0200

    Destination backup

---
+++ BackupDaemon/BackupDaemon/Backup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupDaemon
+{
+    public class Backup
+    {
+        private Destination Destination { get; set; }
+        public Backup(/*Destination*/)
+        {
+
+        }
+
+        public void SSHbackup()
+        {
+
+        }
+        public void FTPbackup()
+        {
+
+        }
+        public void NETbackup()
+        {
+
+        }
+        
+
+    }
+}
