64e60a8115dd0f1a30b3864eda9b391b59df5bd9
temp_user <temp_user@NB-ALIENM14XR2>
2017-05-15 10:43:03 +0200

    Job Solver add. Jobs and triggers soon to come

---
+++ BackupDaemon/BackupDaemon/JobSolver.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackupDaemon
+{
+    public static class JobSolver
+    {
+        //private List<ServerReference.tbTask> Tasks { get; set; }
+
+        private static List<ServerReference.tbTask> Tasks { get; set; }
+
+
+        static void RefreshTasks(List<ServerReference.tbTask> NewTasks)
+        {
+            foreach(ServerReference.tbTask task in NewTasks)
+            {
+                if(!Tasks.Contains(task))
+                {
+                    Tasks.Add(task);
+                }
+            }
+            ResolveTasks();
+        }
+        static void ResolveTasks()
+        {
+
+        }
+    }
+}
